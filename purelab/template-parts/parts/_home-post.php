fa2d31df7b7187d9bd79087c0ec27530b7d1ca87
Danil <rrekkii@gmail.com>
2022-06-27 19:37:04 +0300

    adaptive post section

--- purelab/template-parts/parts/_home-post.php
+++ purelab/template-parts/parts/_home-post.php
@@ -1,16 +1,16 @@
-<div class="section__post-row row align-items-center">
+<div class="section__post-row row align-items-center text-center text-lg-start">
 
-<div class="col-6">
-	<div class="section__post-row-category"><?php the_category(); ?></div>
-	<div class="section__post-row-title"><h2><?php the_title() ?></h2></div>
-	<div class="section__post-row-content"><?php the_content(); ?></div>
-	<div class="section__post-row-link">
-		<a href="<?php the_permalink(); ?>"
-		   class="button btn"><?= __( 'Read more', 'purelab' ) ?></a>
+	<div class=" col-12 col-lg-6 order-2">
+		<div class="section__post-row-category"><?php the_category(); ?></div>
+		<div class="section__post-row-title"><h2><?php the_title() ?></h2></div>
+		<div class="section__post-row-content"><?php the_content(); ?></div>
+		<div class="section__post-row-link">
+			<a href="<?php the_permalink(); ?>"
+			   class="button btn"><?= __( 'Read more', 'purelab' ) ?></a>
+		</div>
 	</div>
-</div>
-<div class="col-6">
-	<img src="<?php the_post_thumbnail_url() ?>" alt="<?php the_title() ?>"
-		 class="w-100 h-auto">
-</div>
+	<div class=" col-12 col-lg-6  mb-5 mb-lg-0  p-0 p-lg-2 order-1">
+		<img src="<?php the_post_thumbnail_url() ?>" alt="<?php the_title() ?>"
+			 class="w-100 h-auto">
+	</div>
 </div>
