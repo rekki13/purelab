2f6d5d1ecfa579fcbb0268de3bfd452c1156a7f3
Danil <rrekkii@gmail.com>
2022-06-27 19:20:11 +0300

    adaptive footer

--- purelab/inc/_custom-footer.php
+++ purelab/inc/_custom-footer.php
@@ -32,24 +32,24 @@ function purelab_footer_TagFooterInner() {
 	$instagramIcon = get_stylesheet_directory() . '/assets/userfilles/icons/instagram.svg';
 	?>
 		<div class="row ">
-			<div class="col-lg-3">
-				<a href="/" class="footer__logo d-flex align-items-center w-auto text-decoration-none">
+			<div class="col-lg-3 col-12 mb-lg-0 mb-5">
+				<a href="/" class="footer__logo d-flex align-items-center w-auto text-decoration-none justify-content-center justify-content-lg-start ">
 					<img class="bi me-2" src="<?= wp_get_attachment_url( get_option( 'media_selector_attachment_id' ) )?>" alt="Logo">
 					<span class="fs-4"><b><?=__('Nature','purelab')?></b> &nbsp;<?=__('Agency','purelab')?></span>
 				</a>
-				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.</p>
-				<ul class="list-group list-socials list-group-horizontal">
+				<p class="text-center text-lg-start">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.</p>
+				<ul class="list-group list-socials list-group-horizontal justify-content-center justify-content-lg-start">
 					<li class="list-group-item bg-transparent border-0"><a href="<?=$facebookURL?>" class="footer__socials footer__socials-facebook"><?=file_get_contents($facebookIcon)?></a></li>
 					<li class="list-group-item bg-transparent border-0"><a href="<?=$instagramURL?>" class="footer__socials footer__socials-instagram"><?=file_get_contents($instagramIcon)?></a></li>
 					<li class="list-group-item bg-transparent border-0"><a href="<?=$linkedInURL?>" class="footer__socials footer__socials-linkedin"><?=file_get_contents($linkedInIcon)?></a></li>
 				</ul>
 			</div>
 
-			<div class="col">
+			<div class="col d-none d-lg-block">
 
 			</div>
 
-			<div class="col-lg-2">
+			<div class="col-lg-2 col-6 col-sm-3 mb-lg-0 mb-5">
 				<h5>Lorem ipsum</h5>
 				<ul class="nav flex-column">
 					<li class="nav-item "><a href="#" class="nav-link p-0 ">Lorem ipsum</a></li>
@@ -58,17 +58,7 @@ function purelab_footer_TagFooterInner() {
 					<li class="nav-item "><a href="#" class="nav-link p-0 ">Lorem ipsum</a></li>
 				</ul>
 			</div>
-			<div class="col-lg-2">
-				<h5>Lorem ipsum</h5>
-				<ul class="nav flex-column">
-					<li class="nav-item "><a href="#" class="nav-link p-0 ">Lorem ipsum</a></li>
-					<li class="nav-item "><a href="#" class="nav-link p-0 ">Lorem ipsum</a></li>
-					<li class="nav-item "><a href="#" class="nav-link p-0 ">Lorem ipsum</a></li>
-					<li class="nav-item "><a href="#" class="nav-link p-0 ">Lorem ipsum</a></li>
-				</ul>
-			</div>
-
-			<div class="col-lg-2">
+			<div class="col-lg-2 col-6 col-sm-3  mb-lg-0 mb-5">
 				<h5>Lorem ipsum</h5>
 				<ul class="nav flex-column">
 					<li class="nav-item "><a href="#" class="nav-link p-0 ">Lorem ipsum</a></li>
@@ -78,7 +68,17 @@ function purelab_footer_TagFooterInner() {
 				</ul>
 			</div>
 
-			<div class="col ">
+			<div class="col-lg-2 col-6 col-sm-3">
+				<h5>Lorem ipsum</h5>
+				<ul class="nav flex-column">
+					<li class="nav-item "><a href="#" class="nav-link p-0 ">Lorem ipsum</a></li>
+					<li class="nav-item "><a href="#" class="nav-link p-0 ">Lorem ipsum</a></li>
+					<li class="nav-item "><a href="#" class="nav-link p-0 ">Lorem ipsum</a></li>
+					<li class="nav-item "><a href="#" class="nav-link p-0 ">Lorem ipsum</a></li>
+				</ul>
+			</div>
+
+			<div class="col-lg-2 col-6 col-sm-3">
 				<h5>Lorem ipsum</h5>
 				<ul class="nav flex-column">
 					<li class="nav-item "><a href="#" class="nav-link p-0 ">Lorem ipsum</a></li>
